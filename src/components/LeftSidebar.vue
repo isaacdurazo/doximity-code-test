dc6ecd1855aec241a59b83bbd5b622f8dc1dd9ba
Isaac <isaacdurazo@gmail.com>
2024-12-09 21:40:25 -0700

    Styling progress

--- src/components/LeftSidebar.vue
+++ src/components/LeftSidebar.vue
@@ -2,12 +2,14 @@
   <div class="left-sidebar">
     <h2>Hello World</h2>
     <p>This is some regular text</p>
-    <button>Let's Go</button>
-    <select>
-      <option>Option 1</option>
-      <option>Option 2</option>
-      <option>Option 3</option>
-    </select>
+    <button class="cta">Let's Go</button>
+    <div class="custom-select">  
+      <select>
+        <option value="0">Option 1</option>
+        <option value="1">Option 2</option>
+        <option value="2">Option 3</option>
+      </select>
+    </div>
   </div>
 
 </template>
