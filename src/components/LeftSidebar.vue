ecf08742b7ad9896a9b6bc6c79f9ef2a0956298f
Isaac <isaacdurazo@gmail.com>
2024-12-10 09:58:15 -0700

    Custom CSS dropdown

--- src/components/LeftSidebar.vue
+++ src/components/LeftSidebar.vue
@@ -3,12 +3,13 @@
     <h2>Hello World</h2>
     <p>This is some regular text</p>
     <button type="button">Let's Go</button>
-    <div class="custom-select">  
-      <select>
-        <option value="0">Option 1</option>
-        <option value="1">Option 2</option>
-        <option value="2">Option 3</option>
-      </select>
+    <div class="dropdown">
+      <button class="dropbtn"><span class="label">Option 1</span><img class="arrow-down" src="@/components/icons/iconArrowdown.svg"></button>
+      <div class="dropdown-content">
+        <a href="#">Option 1</a>
+        <a href="#">Option 2</a>
+        <a href="#">Option 3</a>
+      </div>
     </div>
   </div>
 </template>
